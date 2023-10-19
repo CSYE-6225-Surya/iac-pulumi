bec3d38d25237fcb9ca153886745c30eae34f2d0
Sai Surya Teja Swami <swami.s@northeastern.edu>
2023-10-18 20:40:22 -0400

    assignment-05 changes

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const config = new pulumi.Config("iac-pulumi");
 
 const vpcCidrBlock = config.require("vpcCidrBlock");
 const destinationCidrBlock = config.require("destinationCidrBlock");
+const amiId = config.require("amiId");
+const instanceType = config.require("instanceType");
+const keyName = config.require("keyName");
+const volumeSize = config.require("volumeSize");
+const volumeType = config.require("volumeType");
+const ingressPorts = JSON.parse(config.require("ingressPorts"));
 
 async function main() {
 
@@ -110,6 +116,54 @@ async function main() {
         }
 
         exports.privateRouteTableId = privateRouteTable.id;
+        let ingressArray = [];
+        for (let i = 0; i < ingressPorts.length; i++) {
+            let obj = {
+                description: "Rules",
+                fromPort: 0,
+                toPort: 0,
+                protocol: "tcp",
+                cidrBlocks: [destinationCidrBlock],
+            };
+            obj.fromPort = parseInt(ingressPorts[i]);
+            obj.toPort = parseInt(ingressPorts[i]);
+            ingressArray.push(obj);
+            obj = {};
+        }
+        // Create an EC2 security group
+        const appSecurityGroup = new aws.ec2.SecurityGroup("application security group", {
+            description: "Security group for web application EC2 instances",
+            vpcId: vpc.id, // Replace with your VPC ID
+            ingress: ingressArray,
+        });
+
+        // Create an EC2 instance associated with the security group
+        const ec2Instance = new aws.ec2.Instance("myInstance", {
+            ami: amiId, // Replace with your custom AMI ID
+            instanceType: instanceType, // Replace with your desired instance type
+            securityGroups: [appSecurityGroup.id], // Associate the security group with the instance
+            keyName: keyName,
+            tags: {
+                Name: "MyEC2Instance", // Replace with a meaningful name
+            },
+            rootBlockDevice: {
+                volumeSize: volumeSize,
+                volumeType: volumeType,
+                deleteOnTermination: true,
+            },
+            creditSpecification: {
+                cpuCredits: "standard",
+            },
+            disableApiTermination: false,
+            ebsOptimized: false,
+            instanceInitiatedShutdownBehavior: "stop",
+            subnetId: publicSubnets[0].id, // Replace with the ID of the subnet in your VPC
+        });
+
+        // Export the security group ID and instance ID
+        exports.appSecurityGroupId = appSecurityGroup.id;
+        exports.instanceId = ec2Instance.id;
+
 
     } catch (error) {
         console.error("Error creating resources:", error);
