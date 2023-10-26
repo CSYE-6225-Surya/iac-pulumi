6290f3e0ac76e40471f275e5ba92dbbca7722dad
Sai Surya Teja Swami <swami.s@northeastern.edu>
2023-10-25 20:40:35 -0400

    assignment-06 changes

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ const instanceType = config.require("instanceType");
 const keyName = config.require("keyName");
 const volumeSize = config.require("volumeSize");
 const volumeType = config.require("volumeType");
+const dbPort = config.require("dbPort");
+const dbFamily = config.require("dbFamily");
 const ingressPorts = JSON.parse(config.require("ingressPorts"));
+const dbAllocatedStorage = config.require("dbAllocatedStorage");
+const dbBackupRetentionPeriod = config.require("dbBackupRetentionPeriod");
+const dbInstanceIndentifier = config.require("dbInstanceIndentifier");
+const dbEngine = config.require("dbEngine");
+const dbEngineVersion = config.require("dbEngineVersion");
+const dbInstanceClass = config.require("dbInstanceClass");
+const dbMultiAz = config.require("dbMultiAz");
+const dbName = config.require("dbName");
+const dbPassword = config.require("dbPassword");
+const dbSkipFinalSnapshot = config.require("dbSkipFinalSnapshot");
+const dbUsername = config.require("dbUsername");
+const dbPubliclyAccessible = config.require("dbPubliclyAccessible");
+const dbStorageType = config.require("dbStorageType");
 
 async function main() {
 
@@ -60,6 +75,11 @@ async function main() {
             });
             privateSubnets.push(privateSubnet);
         }
+
+        const dbSubnetGroup = new aws.rds.SubnetGroup("my-db-subnet-group", {
+            name: "my-db-subnet-group",
+            subnetIds: privateSubnets.map(subnet => subnet.id), // Replace with your subnet IDs
+        });
 
         // Create a Public Route Table
         const publicRouteTable = new aws.ec2.RouteTable('public-route-table', {
@@ -135,13 +155,83 @@ async function main() {
             description: "Security group for web application EC2 instances",
             vpcId: vpc.id, // Replace with your VPC ID
             ingress: ingressArray,
+            egress: [
+                {
+                    cidrBlocks: [destinationCidrBlock],
+                    protocol: "-1",
+                    fromPort: 0,
+                    toPort: 0,
+                }
+            ],
+
+        });
+
+        const databaseSecurityGroup = new aws.ec2.SecurityGroup("database security group", {
+            description: "Security group for RDS instances",
+            vpcId: vpc.id,
+            ingress: [
+                {
+                    description: "DB Rule",
+                    fromPort: dbPort,
+                    toPort: dbPort,
+                    protocol: "tcp",
+                    securityGroups: [appSecurityGroup.id],
+                }
+            ],
+        }, { dependsOn: appSecurityGroup });
+
+        const dbParameterGroup = new aws.rds.ParameterGroup("my-db-parameter-group", {
+            family: dbFamily, // Replace with the appropriate PostgreSQL family
+            description: "My custom PostgreSQL DB parameter group",
+            parameters: [
+            ],
+        });
+
+        exports.dbParameterGroupName = dbParameterGroup.name;
+
+        // Define the RDS instance
+        const rdsInstance = new aws.rds.Instance("csye6225-rds-instance", {
+            allocatedStorage: dbAllocatedStorage, // Storage in GB (gp3 type)
+            backupRetentionPeriod: dbBackupRetentionPeriod,
+            dbInstanceIdentifier: dbInstanceIndentifier,
+            engine: dbEngine, // PostgreSQL database engine
+            engineVersion: dbEngineVersion, // Replace with the PostgreSQL version you want
+            instanceClass: dbInstanceClass, // Choose an appropriate instance class
+            multiAz: dbMultiAz,
+            name: dbName,
+            password: dbPassword, // Replace with your password
+            skipFinalSnapshot: dbSkipFinalSnapshot,
+            username: dbUsername,
+            publiclyAccessible: dbPubliclyAccessible, // No public accessibility
+            dbSubnetGroupName: dbSubnetGroup, // Replace with your subnet group name
+            vpcSecurityGroupIds: [databaseSecurityGroup.id], // Replace with your security group ID
+            parameterGroupName: dbParameterGroup.name, // Replace with your parameter group name
+            storageType: dbStorageType,
+            tags: {
+                Name: "csye6225-rds-instance",
+            },
+        }, { dependsOn: dbSubnetGroup });
+
+        const userDataScript = pulumi.all([rdsInstance.dbName, rdsInstance.username, rdsInstance.password, rdsInstance.address]).apply(([dbname, dbusername, dbpassword, dbhost]) => {
+            return `#!/bin/bash
+            echo "DB_NAME=${dbname}" >> /opt/webapp/.env
+            echo "DB_USER=${dbusername}" >> /opt/webapp/.env
+            echo "DB_PASSWORD=${dbpassword}" >> /opt/webapp/.env
+            echo "DB_HOST=${dbhost}" >> /opt/webapp/.env
+            echo "DB_PORT=${dbPort}" >> /opt/webapp/.env
+            cd /opt/webapp
+            node changeConfig.js
+            npm run migrate
+            sudo systemctl enable webapp.service
+            sudo systemctl start webapp.service
+            `;
         });
 
         // Create an EC2 instance associated with the security group
         const ec2Instance = new aws.ec2.Instance("myInstance", {
             ami: amiId, // Replace with your custom AMI ID
             instanceType: instanceType, // Replace with your desired instance type
-            securityGroups: [appSecurityGroup.id], // Associate the security group with the instance
+            vpcSecurityGroupIds: [appSecurityGroup.id], // Associate the security group with the instance
             keyName: keyName,
             tags: {
                 Name: "MyEC2Instance", // Replace with a meaningful name
@@ -158,10 +248,12 @@ async function main() {
             ebsOptimized: false,
             instanceInitiatedShutdownBehavior: "stop",
             subnetId: publicSubnets[0].id, // Replace with the ID of the subnet in your VPC
-        });
+            userData: pulumi.interpolate`${userDataScript}`,
+        }, { dependsOn: [rdsInstance, databaseSecurityGroup] });
 
         // Export the security group ID and instance ID
         exports.appSecurityGroupId = appSecurityGroup.id;
+        exports.databaseSecurityGroupId = databaseSecurityGroup.id;
         exports.instanceId = ec2Instance.id;
 
 
